3fffd39e3eb424d5e156bfdcd503fa8eb7eb0e36
vishnu42 <vrajula@cs.stonybrook.edu>
2015-11-29 22:29:05 -0500

    parameterized calling

--- har_file_generator.py
+++ har_file_generator.py
@@ -1,40 +1,48 @@
+import argparse
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-profile = webdriver.FirefoxProfile()
-profile.add_extension(extension='firebug-2.0.12.xpi')
-profile.add_extension(extension='netExport-0.9b7.xpi')
+def setup_profile(url, logdir):
+	profile = webdriver.FirefoxProfile()
+	profile.add_extension(extension='firebug-2.0.12.xpi')
+	profile.add_extension(extension='netExport-0.9b7.xpi')
+	
+	profile.set_preference("browser.cache.disk.enable", False)
+	domain = "extensions.firebug.";
+	
+	# Set default Firebug preferences
+	profile.set_preference(domain + "currentVersion", "2.0.12")
+	profile.set_preference(domain + "allPagesActivation", "on")
+	profile.set_preference(domain + "defaultPanelName", "net")
+	profile.set_preference(domain + "net.enableSites", True)
+	
+	# Set default NetExport preferences
+	profile.set_preference(domain + "netexport.alwaysEnableAutoExport", True)
+	profile.set_preference(domain + "netexport.defaultFileName", url.split("/")[-2] + "-%Y-%m-%d")
+	profile.set_preference(domain + "netexport.showPreview", False)
+	profile.set_preference(domain + "netexport.defaultLogDir", logdir)
+	
+	return profile
 
-profile.set_preference("browser.cache.disk.enable", False)
-domain = "extensions.firebug.";
+def start(url, logdir):
+	for i in range(5):
+		profile = setup_profile(url, logdir)
+		browser = webdriver.Firefox(firefox_profile=profile)
+		time.sleep(5)
+		browser.get(url)
+		time.sleep(5)
+		browser.close()
+	
 
-# Set default Firebug preferences
-profile.set_preference(domain + "currentVersion", "2.0.12")
-profile.set_preference(domain + "allPagesActivation", "on")
-profile.set_preference(domain + "defaultPanelName", "net")
-profile.set_preference(domain + "net.enableSites", True)
+def main():
+	parser = argparse.ArgumentParser(description="Open webpages and trigger net export ")
+	parser.add_argument('--url', help="url to be loaded")
+	parser.add_argument('--logdir', help = ".har file log location")
+	#parser.add_argument('--threads', type=int, default=10, help="number of threads to use")
+	args = parser.parse_args()
+	#read_graphs(args.input, args.output, args.k, args.threads)
+	start(args.url, args.logdir)
 
-# Set default NetExport preferences
-profile.set_preference(domain + "netexport.alwaysEnableAutoExport", True)
-profile.set_preference(domain + "netexport.showPreview", False)
-profile.set_preference(domain + "netexport.defaultLogDir", "/Users/Anirudh/Desktop/570/project/output/")
-
-browser = webdriver.Firefox(firefox_profile=profile)
-time.sleep(5)
-for i in range(10):
-	browser.get("10.245.206.250/NDTV/")
-	time.sleep(6)
-time.sleep(5)
-browser.close()
-
-
-'''
-try: 
-    #elem = browser.find_element_by_xpath("/html/body/div[2]/div[2]/table/tbody/tr/td[3]/a")
-    elem = browser.find_element_by_link_text("SEARCH")
-    elem.send_keys(Keys.RETURN)
-
-except NoSuchElementException:
-    assert 0, "can't find XPATH or Link Name"
-'''
+if __name__ == "__main__":
+	main()
